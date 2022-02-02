032290bcb22fa12317aec333e92454e64adc3244
Your global username <your@email.com>
2022-02-02 09:50:27 -0300

    install axios

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class App extends Component {
   constructor() {
     super()
-    // GET , POST, PUT, PATCH; DELETE
 
+    //libreria Axios entrega mayor candidad de datos y responde todo en promesas además de tratar de resolver eltipo de datos por si solo.
+    
     //GET
-    /*
-    fetch(' https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(json => console.table(json))
-  */
+    //axios.get('https://jsonplaceholder.typicode.com/users')
+    //destructuring de nodo data 
+    //.then(({ data }) => console.log(data))
+    
     //POST
+    axios.post('https://jsonplaceholder.typicode.com/users',
+    {name: 'nombre',
+    username:'Usuario'}
+    ).then(({ data }) => console.log(data))
 
-    fetch(' https://jsonplaceholder.typicode.com/users', {
+
+    // GET , POST, PUT, PATCH; DELETE
+    //GET
+    /*  fetch('https://jsonplaceholder.typicode.com/users')
+         .then(response => response.json())
+         .then(json => console.log(json))
+      */
+
+    //POST
+    /*fetch(' https://jsonplaceholder.typicode.com/users', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -24,12 +38,35 @@ export default class App extends Component {
       })
     })
       .then(response => response.json())
-      .then(json => console.table(json))
+      .then(json => console.table(json))*/
+
+
+    //AXIOS
+
+
+
   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   render() {
     return (<div>
       <h1>Ajax</h1>
     </div>);
   }
+
+
 }
